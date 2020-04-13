fe777627ed748eda880f3fa4387f85fa83c669d5
moongurt <48335482+moongurt@users.noreply.github.com>
2020-04-13 09:57:18 -0400

    added circle motor test

---
+++ WireFollowingSensor/circleMotorTest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Example usage of the ODrive python library to monitor and control ODrive devices
+"""
+
+from __future__ import print_function
+
+import odrive
+from odrive.enums import *
+import time
+import math
+
+# Set Up
+print("finding an odrive...")
+my_drive = odrive.find_any()
+my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+##
+
+# quick test
+my_drive.axis0.controller.pos_setpoint = 10000
+
+## circle
+my_drive.axis0.controller.config.setpoints_in_cpr = True
+##
