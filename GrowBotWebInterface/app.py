e041fb5eb9db34131654401907ca71894e600ec2
Wilson Holmes <wilsonjholmes@gmail.com>
2020-04-16 00:31:23 -0400

    fixed query?

--- GrowBotWebInterface/app.py
+++ GrowBotWebInterface/app.py
@@ -44,7 +44,7 @@ admin.add_view(ModelView(model.SensorReading))
 def db2csv():
     conn = sqlite3.connect('growbot.db')
     curs = conn.cursor()
-    curs.execute("select * from all;")
+    curs.execute("select * from sensor;")
     m_dict = list(curs.fetchall())
 
     with open("growbot.csv", "wb") as f:
