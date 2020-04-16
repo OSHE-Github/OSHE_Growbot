42781e9d98dfb1bb21c4c05216bf602192e49a60
Wilson Holmes <wilsonjholmes@gmail.com>
2020-04-15 21:58:54 -0400

    fixed naming issues

--- GrowBotWebInterface/app.py
+++ GrowBotWebInterface/app.py
@@ -56,7 +56,7 @@ def gen(camera):
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
-@app.route('/videofeed')
+@app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
