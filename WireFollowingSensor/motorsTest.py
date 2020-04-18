a95067cf57cdc833c1d72ab221846940b4b840c5
moongurt <48335482+moongurt@users.noreply.github.com>
2020-04-17 20:13:04 -0400

    Added circular mode for both motors

--- WireFollowingSensor/motorsTest.py
+++ WireFollowingSensor/motorsTest.py
@@ -20,34 +20,46 @@ my_drive = odrive.find_any()
 # Calibrate motor and wait for it to finish
 print("starting calibration...")
 my_drive.axis0.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
+my_drive.axis1.requested_state = AXIS_STATE_FULL_CALIBRATION_SEQUENCE
 while my_drive.axis0.current_state != AXIS_STATE_IDLE:
+    time.sleep(0.1)
+while my_drive.axis1.current_state != AXIS_STATE_IDLE:
     time.sleep(0.1)
 
 my_drive.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+my_drive.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
 
 # To read a value, simply read the property
 print("Bus voltage is " + str(my_drive.vbus_voltage) + "V")
 
 # Or to change a value, just assign to the property
 my_drive.axis0.controller.pos_setpoint = 3.14
-print("Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
+my_drive.axis1.controller.pos_setpoint = 3.14
+print("1: Position setpoint is " + str(my_drive.axis0.controller.pos_setpoint))
+print("2: Position setpoint is " + str(my_drive.axis1.controller.pos_setpoint))
 
 # And this is how function calls are done:
 for i in [1,2,3,4]:
     print('voltage on GPIO{} is {} Volt'.format(i, my_drive.get_adc_voltage(i)))
 
+# circular continuous running
+my_drive.axis0.controller.config.setpoints_in_cpr = True
+my_drive.axis1.controller.config.setpoints_in_cpr = True
+
 # A sine wave to test
 t0 = time.monotonic()
+setpoint = 0
 while True:
-    setpoint = 10000.0 * math.sin((time.monotonic() - t0)*2)
+    setpoint = setpoint + 100
     print("goto " + str(int(setpoint)))
     my_drive.axis0.controller.pos_setpoint = setpoint
+    my_drive.axis1.controller.pos_setpoint = setpoint
     time.sleep(0.01)
 
 # Some more things you can try:
 
 # Write to a read-only property:
-my_drive.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
+#my_drive.vbus_voltage = 11.0  # fails with `AttributeError: can't set attribute`
 
 # Assign an incompatible value:
-my_drive.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
+#my_drive.motor0.pos_setpoint = "I like trains"  # fails with `ValueError: could not convert string to float`
