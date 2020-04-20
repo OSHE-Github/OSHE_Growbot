42a43c3420333043c13b6d874a4f84c413e8c733
Wilson Holmes <wilsonjholmes@gmail.com>
2020-04-20 11:43:34 -0400

    added gassensor.py to startup script

--- GrowBotWebInterface/setup.py
+++ GrowBotWebInterface/setup.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("python3 SoilSensorReadOnly.py & python3 TempHumidPressSensor.py & python3 nineDofSensor.py", shell=True)
+subprocess.run("python3 SoilSensorReadOnly.py & python3 TempHumidPressSensor.py & python3 nineDofSensor.py & python3 GasSensor.py", shell=True)
