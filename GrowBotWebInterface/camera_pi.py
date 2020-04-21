81bff15f7bb51a7b2e9bb9aa208df8e9fcb54663
wilsonjholmes <59266497+wilsonjholmes@users.noreply.github.com>
2020-04-21 15:12:59 -0400

    changed to 1080p

--- GrowBotWebInterface/camera_pi.py
+++ GrowBotWebInterface/camera_pi.py
@@ -35,7 +35,7 @@ class Camera(object):
     def _thread(cls):
         with picamera.PiCamera() as camera:
             # camera setup
-            camera.resolution = (3840, 2160) #320, 240
+            camera.resolution = (1920, 1080) #320, 240
             camera.hflip = True
             camera.vflip = True
 
