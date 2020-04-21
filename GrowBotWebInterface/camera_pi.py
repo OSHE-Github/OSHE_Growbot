cc97c881d5077b639643725609d70ac9f1378c5c
wilsonjholmes <59266497+wilsonjholmes@users.noreply.github.com>
2020-04-21 15:11:43 -0400

    testing 4k camera stream

--- GrowBotWebInterface/camera_pi.py
+++ GrowBotWebInterface/camera_pi.py
@@ -35,7 +35,7 @@ class Camera(object):
     def _thread(cls):
         with picamera.PiCamera() as camera:
             # camera setup
-            camera.resolution = (720, 480) #320, 240
+            camera.resolution = (3840, 2160) #320, 240
             camera.hflip = True
             camera.vflip = True
 
