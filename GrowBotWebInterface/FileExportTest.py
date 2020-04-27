4c4bb3ced1ba164cf0a8751718daafeefb8daca5
Wilson Holmes <wilsonjholmes@gmail.com>
2020-04-26 20:41:30 -0400

    finalized email sending code email sending code has been tested to work, but hasnt been tested within the chron scheduling part of the app.py file

--- GrowBotWebInterface/FileExportTest.py
+++ GrowBotWebInterface/FileExportTest.py
@@ -5,23 +5,20 @@ from email.message import EmailMessage
 
 # Adds a timestamp to the filenames
 reading_time = datetime.datetime.now()
-sensors = reading_time.strftime('%m-%d-%Y') + '_sensors.csv'
-sensorreadings = reading_time.strftime('%m-%d-%Y') + '_sensorreadings.csv'
+readDate = reading_time.strftime('%m-%d-%Y')
+sensors = readDate + '_sensors.csv'
+sensorreadings = readDate + '_sensorreadings.csv'
 
-print(sensors)
-print(sensorreadings)
-# os.rename('sensors.csv', sensors)
-# os.rename('sensorreadings.csv', sensorreadings)
-
-
+# Sends an email with .csv attachment
 msg = EmailMessage()
 msg["From"] = 'OSHETesting@gmail.com'
-msg["Subject"] = reading_time.strftime('%m-%d-%Y') + 's files'
-msg["To"] = 'wilsonh@mtu.edu'
-msg.set_content("Here are the .csv files of the Growbot's databse tables from " + reading_time.strftime('%m-%d-%Y'))
-msg.add_attachment(open('04-26-2020_sensors.csv', "r").read(), filename="04-26-2020_sensors.csv")
-msg.add_attachment(open('04-26-2020_sensors.csv', "r").read(), filename="04-26-2020_sensorreadings.csv")
+msg["Subject"] = readDate + ' *.csv files'
+msg["To"] = 'wilsonh@mtu.edu'   # Put your email here
+msg.set_content("Here are the .csv files of the Growbot's databse tables from " + readDate)
+msg.add_attachment(open('sensors.csv', "r").read(), filename=sensors)
+msg.add_attachment(open('sensorreadings.csv', "r").read(), filename=sensorreadings)
 
+# Sends the email
 s = smtplib.SMTP_SSL('smtp.gmail.com')
-s.login('OSHETesting', 'opensource')
+s.login('OSHETesting@gmail.com', 'opensource')  # Email I created and used for testing. Have to turn on support for less secure apps.
 s.send_message(msg)
--- GrowBotWebInterface/app.py
+++ GrowBotWebInterface/app.py
@@ -15,6 +15,9 @@ import csv
 import atexit
 from glob import glob; from os.path import expanduser
 import pandas as pd
+# For sending emails
+import smtplib
+from email.message import EmailMessage
 # Flask
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, send_from_directory, jsonify, make_response, flash, Response
 # WTForms
@@ -43,7 +46,7 @@ admin.add_view(ModelView(model.Sensor))
 admin.add_view(ModelView(model.SensorReading))
 
 
-# Function to save data to csv every day and clear database
+# Function to save data to csv every day, clear database, and email data
 def db2csv():
     # Connect to database and make cursor
     conn = sqlite3.connect(glob(expanduser('growbot.db'))[0])
@@ -69,6 +72,26 @@ def db2csv():
     conn.commit()
     #close the connection
     conn.close()
+
+    # Adds a timestamp to the filenames
+    reading_time = datetime.datetime.now()
+    readDate = reading_time.strftime('%m-%d-%Y')
+    sensors = readDate + '_sensors.csv'
+    sensorreadings = readDate + '_sensorreadings.csv'
+
+    # Sends an email with .csv attachment
+    msg = EmailMessage()
+    msg["From"] = 'OSHETesting@gmail.com'
+    msg["Subject"] = readDate + ' *.csv files'
+    msg["To"] = 'wilsonh@mtu.edu'   # Put your email here
+    msg.set_content("Here are the .csv files of the Growbot's databse tables from " + readDate)
+    msg.add_attachment(open('sensors.csv', "r").read(), filename=sensors)
+    msg.add_attachment(open('sensorreadings.csv', "r").read(), filename=sensorreadings)
+
+    # Sends the email
+    s = smtplib.SMTP_SSL('smtp.gmail.com')
+    s.login('OSHETesting@gmail.com', 'opensource')  # Email I created and used for testing. Have to turn on support for less secure apps.
+    s.send_message(msg)
 
 
 # create schedule for exporting database to csv once every day at 1am
