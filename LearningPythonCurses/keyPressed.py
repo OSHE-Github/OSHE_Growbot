7bef70be74772cf16cdc1d293c339dfbbc8b49f5
Wilson Holmes <wilsonjholmes@gmail.com>
2020-09-30 15:56:19 -0400

    Moving to my desktop to get more screen realestate and a mouse for the time being.

--- LearningPythonCurses/keyPressed.py
+++ LearningPythonCurses/keyPressed.py
@@ -12,6 +12,9 @@ def main(main_screen):
     # Prints a line in the center of the specified screen
     def print_center(message, screen):
         
+        # Get the number of rows and columns so that text can be centered in the terminal window.
+        num_rows, num_cols = screen.getmaxyx()
+        
         # Calculate center row
         middle_row = int(num_rows / 2)
 
@@ -24,21 +27,60 @@ def main(main_screen):
         # Draw the text
         screen.addstr(middle_row, x_position, message)
         screen.refresh()
+        
+    # Prints a border on the specified window
+    def print_border(window):
+        
+        # Get the number of rows and columns so that text can be centered in the terminal window.
+        num_rows, num_cols = window.getmaxyx()
+        
+        horizontal_border = "─"*(num_cols-2)
+        vertical_border = "│"*(num_rows-2)
+        top_left_corner_border = "┌"
+        top_right_corner_border = "┐"
+        bottom_right_corner_border = "┘"
+        bottom_left_corner_border = "└"
+
+        # Draw the top row
+        top_border = top_left_corner_border+horizontal_border+top_right_corner_border
+        window.addstr(0, 0, top_border)
+        window.refresh()
 
     # Ask the user for input.
     print_center("Press any key...", main_screen)
+    
+    
+    # lines, columns, start line, start column
+    quit_dialog = curses.newwin(5, 30, 0, 0)
+    
+    # lines, columns, start line, start column
+    window_border = curses.newwin(num_cols, num_rows, 0, 0)
+    
+    # lines, columns, start line, start column
+    remote_control_main = curses.newwin(num_cols-2, num_rows-2, 1, 1)
+    
+    quit_dialog.addstr("┌────────────────────────┐\n")
+    quit_dialog.addstr("│ Press `Ctrl+c` to quit │\n")
+    quit_dialog.addstr("└────────────────────────┘\n")
+    quit_dialog.refresh()
 
     # Loop forever to show the keycode and character the user enters.
     while True:
-        c = main_screen.getch()
+        c = remote_control_main.getch()
         
         # Get the number of rows and columns so that text can be centered in the terminal window.
-        num_rows, num_cols = main_screen.getmaxyx()
+        num_rows, num_cols = remote_control_main.getmaxyx()
         
-        main_screen.clear()
+        remote_control_main.clear()
+        
+        quit_dialog.clear()
+        quit_dialog.addstr("┌────────────────────────┐\n")
+        quit_dialog.addstr("│ Press `Ctrl+c` to quit │\n")
+        quit_dialog.addstr("└────────────────────────┘\n")
+        quit_dialog.refresh()
         
         # Convert the key to ASCII and print ordinal value
-        print_center("You pressed %s which is keycode %d." % (chr(c), c), main_screen)
+        print_center("You pressed %s which is keycode %d. " % (chr(c), c), remote_control_main)
 
 wrapper(main)
 
